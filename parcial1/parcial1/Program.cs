4c02e4880e758a4902c48af04ef2514abc102311
andres vallejo varela <andres4513330@gmail.com>
2024-09-13 22:41:22 -0500

    solicita a y b

--- parcial1/parcial1/Program.cs
+++ parcial1/parcial1/Program.cs
@@ -2,6 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(" ");
+
+        // primero solicitamos el coeficiente 
+
+        Console.Write("ingrese el valor de a ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor de b ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+
     }
 }
