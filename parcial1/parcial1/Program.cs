23d6e4dee778d40eaab6de976cf81e7a2d97fbe6
andres vallejo varela <andres4513330@gmail.com>
2024-09-13 22:58:00 -0500

    finnn

--- parcial1/parcial1/Program.cs
+++ parcial1/parcial1/Program.cs
@@ -4,6 +4,12 @@
     {
         Console.WriteLine(" ");
 
+        /*Escriba un programa que pida los coeficientes de una ecuación de primer grado (a x + b = 0) y
+muestre la solución.
+Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los
+números sean solución. La fórmula de las soluciones es x = -b / a
+*/
+
         // primero solicitamos el coeficiente 
 
         Console.Write("ingrese el valor de a ");
